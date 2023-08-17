9590f1bebaeb753d43f6a390263d1bb1c6d12d80
ChrisK91 <chrisgit@outlook.com>
2023-08-17 19:38:39 +0200

    Update postcss.config.js

--- themes/tailwind-theme/assets/css/postcss.config.js
+++ themes/tailwind-theme/assets/css/postcss.config.js
@@ -1,25 +1,5 @@
 const themeDir = __dirname + '/../../';
 
-const purgecss = require('@fullhuman/postcss-purgecss')({
-    // see https://gohugo.io/hugo-pipes/postprocess/#css-purging-with-postcss
-    content: [
-        './hugo_stats.json',
-        themeDir + '/hugo_stats.json',
-        'exampleSite/hugo_stats.json',
-        "./**/*.{html,html,xml,rss,md}",
-    ],
-    safelist: [
-      "[type='checkbox']",
-      "input",
-      "input[type=*]",
-      "[type]"
-    ],
-    defaultExtractor: (content) => {
-        let els = JSON.parse(content).htmlElements;
-        return els.tags.concat(els.classes, els.ids);
-    }
-})
-
 module.exports = {    
     plugins: [        
         require('postcss-import')({
@@ -29,7 +9,7 @@ module.exports = {
         require('autoprefixer')({
             path: [themeDir]
         }),
-        ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : [])
+        ...(process.env.HUGO_ENVIRONMENT === 'production' ? [] : [])
         //...[purgecss]
     ]
 }
